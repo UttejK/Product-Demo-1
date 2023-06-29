eb5bd205102d9ef042ac7125a882f846118a304b
UttejK <uttej09092001@gmail.com>
2023-06-29 21:17:00 +0530

    fix hdr cors issue

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -3,6 +3,7 @@ import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
 import { Canvas } from "@react-three/fiber";
 import { Center, Environment } from "@react-three/drei";
+// import bg from "./postdamer_platz_1k.hdr";
 
 const RenderCanvas = () => {
   return (
@@ -13,7 +14,7 @@ const RenderCanvas = () => {
       className="w-full max-w-full h-full transition-all ease-in"
     >
       <ambientLight intensity={0.5} />
-      <Environment preset="city" />
+      <Environment files="./public/hdris/potsdamer_platz_1k.hdr" />
 
       <CameraRig>
         <Backdrop />
